d9d1a8302439f1c579243aedbaeccd912079610f
Mari Budko <gazibar.marina@gmail.com>
2025-10-02 13:30:33 +0300

    chore: setup data layer
    - MovieDTO added
    - MovieMapper added

---
+++ MovieApp/Data/DTO/MovieDTO.swift
@@ -0,0 +1,23 @@
+//
+//  MovieDTO.swift
+//  MovieApp
+//
+//  Created by Mari Budko on 02.10.2025.
+//
+
+import Foundation
+
+struct MovieDTO: Codable {
+    let id: Int
+    let title: String
+    let overview: String?
+    let poster_path: String?
+    let release_date: String?
+    let vote_average: Double?
+}
+
+struct PagedResponse<T: Codable>: Codable {
+    let page: Int
+    let total_pages: Int
+    let results: [T]
+}
---
+++ MovieApp/Data/Repositories/MovieRepository.swift
@@ -0,0 +1,12 @@
+//
+//  MovieRepository.swift
+//  MovieApp
+//
+//  Created by Mari Budko on 02.10.2025.
+//
+
+import Foundation
+
+final class MovieRepository {
+    
+}
